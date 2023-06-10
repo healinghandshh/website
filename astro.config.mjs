023b0bf9b82f57d28a053fd4cdaf3ee4eb5509ef
messiahsmiley <m.smileybusiness@gmail.com>
2023-06-10 14:37:00 -0400

    Netlify Adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
 import compress from "astro-compress";
-
 import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [compress(), astroImageTools],
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify()
 });
